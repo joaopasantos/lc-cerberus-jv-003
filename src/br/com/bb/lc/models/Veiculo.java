e3026d72dd20883485d95c205b33e23a2013f717
F4957995 Joao Pedro Albuquerque dos Santos <joaopasantos@bb.com.br>
2022-08-01 13:01:04 -0300

    Adiciona verificadores para as entradas de Veiculo

--- src/br/com/bb/lc/models/Veiculo.java
+++ src/br/com/bb/lc/models/Veiculo.java
@@ -7,6 +7,17 @@ public class Veiculo {
     private String placa;
 
     public Veiculo(TipoVeiculo tipo, Marca marca, String modelo, String placa) {
+        if (tipo == null)
+            throw new IllegalArgumentException("Tipo não foi preenchido. Não foi possível concluir a operação.");
+        if (marca == null)
+            throw new IllegalArgumentException("Marca não foi preenchida. Não foi possível concluir a operação.");
+        if (modelo.isBlank() || modelo == null)
+            throw new IllegalArgumentException("Modelo não foi preenchido. Não foi possível concluir a operação.");
+        if (placa.isBlank() || placa == null)
+            throw new IllegalArgumentException("Placa não foi preenchida. Não foi possível concluir a operação.");
+        if (placa.length() < 10)
+            throw new IllegalArgumentException("Placa inválida. Não foi possível concluir a operação.");
+
         this.tipo = tipo;
         this.marca = marca;
         this.modelo = modelo;
